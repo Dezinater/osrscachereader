1fdc2db9f22c43d7f220fc1c316c2144fa707266
JasonM97 <dezinated@gmail.com>
2021-06-02 23:25:46 -0400

    Updated NpcLoader to use the new DataView reading system

--- NpcLoader.js
+++ NpcLoader.js
@@ -6,14 +6,12 @@ class NpcLoader {
 	constructor(bytes) { 
 		this.bytes = bytes;
 		this.def = new NpcDefinition();
-		this.streamPos = 0;
 	}
 	
 	load() {
 		let dataview = new DataView(this.bytes.buffer);
 		do {
-			var opcode = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			var opcode = dataview.readUint8();
 			this.handleOpcode(opcode, dataview);
 		} while(opcode != 0);
 		
@@ -25,69 +23,56 @@ class NpcLoader {
 		var index;
 		if (opcode == 1)
 		{
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			this.def.models = [];
 
 			for (index = 0; index < length; ++index)
 			{
-				this.def.models.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
+				this.def.models.push(dataview.readUint16());
 			}
 		}else if (opcode == 2)
 		{
-			var name = dataview.getString(this.streamPos);
-			this.streamPos += name.length;
+			var name = dataview.readString();
 			this.def.name = name;
 		}
 		else if (opcode == 12)
 		{
-			this.def.size = dataview.getUint8(this.streamPos);
+			this.def.size = dataview.readUint8();
 		}
 		else if (opcode == 13)
 		{
-			this.def.standingAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.standingAnimation = dataview.readUint16();
 		}
 		else if (opcode == 14)
 		{
-			this.def.walkingAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.walkingAnimation = dataview.readUint16();
 		}
 		else if (opcode == 15)
 		{
-			this.def.rotateLeftAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.rotateLeftAnimation = dataview.readUint16();
 		}
 		else if (opcode == 16)
 		{
-			this.def.rotateRightAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.rotateRightAnimation = dataview.readUint16();
 		}
 		else if (opcode == 17)
 		{
-			this.def.walkingAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
-			this.def.rotate180Animation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
-			this.def.rotate90RightAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
-			this.def.rotate90LeftAnimation = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.walkingAnimation = dataview.readUint16();
+			this.def.rotate180Animation = dataview.readUint16();
+			this.def.rotate90RightAnimation = dataview.readUint16();
+			this.def.rotate90LeftAnimation = dataview.readUint16();
 		}
 		else if (opcode == 18)
 		{
-			this.def.category = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.category = dataview.readUint16();
 		}
 		else if (opcode >= 30 && opcode < 35)
 		{
 			if(this.def.actions == undefined)
 				this.def.actions = [];
 			
-			var readString = dataview.getString(this.streamPos);
+			var readString = dataview.readString();
 			this.def.actions[opcode - 30] = readString;
-			this.streamPos += readString.length;
 			
 			if (this.def.actions[opcode - 30] == "Hidden")
 			{
@@ -96,45 +81,38 @@ class NpcLoader {
 		}
 		else if (opcode == 40)
 		{
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			this.def.recolorToFind = [];
 			this.def.recolorToReplace = [];
 
 			for (index = 0; index < length; ++index)
 			{
-				this.def.recolorToFind.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
-				this.def.recolorToReplace.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
+				this.def.recolorToFind.push(dataview.readUint16());
+				this.def.recolorToReplace.push(dataview.readUint16());
 			}
 
 		}
 		else if (opcode == 41)
 		{
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			this.def.retextureToFind = [];
 			this.def.retextureToReplace = [];
 
 			for (index = 0; index < length; ++index)
 			{
-				this.def.retextureToFind.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
-				this.def.retextureToReplace.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
+				this.def.retextureToFind.push(dataview.readUint16());
+				this.def.retextureToReplace.push(dataview.readUint16());
 			}
 
 		}
 		else if (opcode == 60)
 		{
-			length = dataview.getUint8(this.streamPos);
+			length = dataview.readUint8();
 			this.def.chatheadModels = [];
 
 			for (index = 0; index < length; ++index)
 			{
-				this.def.chatheadModels.push(dataview.getUint16(this.streamPos));
-				this.streamPos += 2;
+				this.def.chatheadModels.push(dataview.readUint16());
 			}
 
 		}
@@ -144,18 +122,15 @@ class NpcLoader {
 		}
 		else if (opcode == 95)
 		{
-			this.def.combatLevel = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.combatLevel = dataview.readUint16();
 		}
 		else if (opcode == 97)
 		{
-			this.widthScale = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.widthScale = dataview.readUint16();
 		}
 		else if (opcode == 98)
 		{
-			this.def.heightScale = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.heightScale = dataview.readUint16();
 		}
 		else if (opcode == 99)
 		{
@@ -163,50 +138,42 @@ class NpcLoader {
 		}
 		else if (opcode == 100)
 		{
-			this.def.ambient = dataview.getInt8(this.streamPos);
-			this.streamPos += 1;
+			this.def.ambient = dataview.readInt8();
 		}
 		else if (opcode == 101)
 		{
-			this.def.contrast = dataview.getInt8(this.streamPos);
-			this.streamPos += 1;
+			this.def.contrast = dataview.readInt8();
 		}
 		else if (opcode == 102)
 		{
-			this.def.headIcon = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.headIcon = dataview.readUint16();
 		}
 		else if (opcode == 103)
 		{
-			this.def.rotationSpeed = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.rotationSpeed = dataview.readUint16();
 		}
 		else if (opcode == 106)
 		{
-			this.def.varbitId = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.varbitId = dataview.readUint16();
 			if (this.def.varbitId == 65535)
 			{
 				this.def.varbitId = -1;
 			}
 
-			this.def.varpIndex = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.varpIndex = dataview.readUint16();
 			
 			if (this.def.varpIndex == 65535)
 			{
 				this.def.varpIndex = -1;
 			}
 
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			
 			this.def.configs = new int[length + 2];
 
 			for (index = 0; index <= length; ++index)
 			{
-				this.def.configs[index] = dataview.getUint16(this.streamPos);
-				this.streamPos += 2;
+				this.def.configs[index] = dataview.readUint16();
 				
 				if (this.def.configs[index] == '\uffff')
 				{
@@ -231,38 +198,33 @@ class NpcLoader {
 		}
 		else if (opcode == 118)
 		{
-			this.def.varbitId = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.varbitId = dataview.readUint16();
 			
 			if (this.def.varbitId == 65535)
 			{
 				this.def.varbitId = -1;
 			}
 
-			this.def.varpIndex = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			this.def.varpIndex = dataview.readUint16();
 			
 			if (this.def.varpIndex == 65535)
 			{
 				this.def.varpIndex = -1;
 			}
 
-			var varVal = dataview.getUint16(this.streamPos);
-			this.streamPos += 2;
+			var varVal = dataview.readUint16();
 			
 			if (varVal == 0xFFFF)
 			{
 				varVal = -1;
 			}
 
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			this.def.configs = [];
 
 			for (index = 0; index <= length; ++index)
 			{
-				var value = dataview.getUint16(this.streamPos);
-				this.streamPos += 2;
+				var value = dataview.readUint16();
 				
 				if (this.def.configs[index] == '\uffff') {
 					this.def.configs.push(-1);
@@ -275,30 +237,25 @@ class NpcLoader {
 		}
 		else if (opcode == 249)
 		{
-			length = dataview.getUint8(this.streamPos);
-			this.streamPos += 1;
+			length = dataview.readUint8();
 			
 			this.def.params = {};
 
 			for (var i = 0; i < length; i++)
 			{
-				var isString = dataview.getUint8(this.streamPos) == 1;
-				this.streamPos += 1;
+				var isString = dataview.readUint8() == 1;
 				
-				var key = dataview.getInt24(this.streamPos);
-				this.streamPos += 3;
+				var key = dataview.readInt24();
 				var value;
 
 				if (isString)
 				{
-					value = dataview.getString(this.streamPos);
-					this.streamPos += value.length;
+					value = dataview.readString();
 				}
 
 				else
 				{
-					value = dataview.getInt32(this.streamPos)
-					this.streamPos += 4;
+					value = dataview.readInt32()
 				}
 
 				this.def.params[key] = value;
