a155043d95cb2f2ba7dc04a84487ea87d2e3dbae
JasonM97 <dezinated@gmail.com>
2021-06-10 01:26:14 -0400

    added itemloader and sequenceloader to configtypes

--- src/cacheReader/cacheTypes/ConfigType.js
+++ src/cacheReader/cacheTypes/ConfigType.js
@@ -1,6 +1,8 @@
 import KitLoader from '../loaders/KitLoader.js'
 import ObjectLoader from '../loaders/ObjectLoader.js'
 import NpcLoader from '../loaders/NpcLoader.js'
+import ItemLoader from '../loaders/ItemLoader.js'
+import SequenceLoader from '../loaders/SequenceLoader.js'
 
 const ConfigType = { 
     UNDERLAY: {id: 1, loader: undefined},
@@ -10,9 +12,9 @@ const ConfigType = {
 	OBJECT: {id: 6, loader: ObjectLoader},
 	ENUM: {id: 8, loader: undefined},
 	NPC: {id: 9, loader: NpcLoader},
-	ITEM: {id: 10, loader: undefined},
+	ITEM: {id: 10, loader: ItemLoader},
 	PARAMS: {id: 11, loader: undefined},
-	SEQUENCE: {id: 12, loader: undefined},
+	SEQUENCE: {id: 12, loader: SequenceLoader},
 	SPOTANIM: {id: 13, loader: undefined},
 	VARBIT: {id: 14, loader: undefined},
 	VARCLIENT: {id: 19, loader: undefined},
