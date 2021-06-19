f0832ee140abb881143e429564e70693b9b07cee
JasonM97 <dezinated@gmail.com>
2021-06-19 01:42:19 -0400

    added web browser caching for the .dat2 file

--- src/cacheReader/CacheRequester.js
+++ src/cacheReader/CacheRequester.js
@@ -6,50 +6,80 @@ import { decompress } from 'bz2';
 export default class CacheRequester {
 	//should be used to make read requests from the cache
 	//this should make it easier to multithread/async this later on
-	constructor(rootDir){
-		this.datDataPromise = Ajax.getFileBytes(rootDir+"main_file_cache.dat2");
-		this.datDataPromise.then((x) => {
-			this.datData = x;
-		});
+	constructor(rootDir) {
+		if ('caches' in window) { //if we are able to use cache api
+			var request = rootDir + "cache/" + "main_file_cache.dat2";
+			this.datDataPromise = caches.open('osrsrenderer').then((browserCache) => {
+				return caches.has(request).then((cacheExists) => {
+					if (!cacheExists) {
+						return browserCache.add(request).then(() => {
+							//needs to be retrieved after its been added otherwise will return undefined
+							return browserCache.match(request)
+								.then((response) => response.blob())
+								.then((blob) => blob.arrayBuffer());
+						});
+					} else {
+						return browserCache.match(request)
+							.then((response) => response.blob())
+							.then((blob) => blob.arrayBuffer());
+					}
+				});
+			});
+
+			this.datDataPromise.then((blobArray) => {
+				this.datData = new Uint8Array(blobArray);
+			});
+
+		} else {
+			this.datDataPromise = Ajax.getFileBytes(rootDir + "main_file_cache.dat2");
+			this.datDataPromise.then((x) => {
+				this.datData = x;
+			});
+		}
+
+
+
 	}
-	
-	readData(index, size, segment, archiveId = 0){
+
+
+
+	readData(index, size, segment, archiveId = 0) {
 		var compressedData = new Uint8Array(size);
-		
+
 		this.readSector(compressedData, segment, archiveId);
-		
+
 		let dataview = new DataView(compressedData.buffer);
 		var compressionOpcode = dataview.getUint8(0);
 		var compressedLength = dataview.getUint32(1);
 
 		var data;
 		var decompressedData;
-		
-		if(compressionOpcode == 0) { //none
-			data = new Uint8Array(dataview.buffer.slice(5,9+compressedLength));
+
+		if (compressionOpcode == 0) { //none
+			data = new Uint8Array(dataview.buffer.slice(5, 9 + compressedLength));
 			decompressedData = data;
 			index.revision = dataview.getUint16(data.buffer.byteLength)
-		} else if(compressionOpcode == 1) { //bz2
-			data = new Uint8Array(dataview.buffer.slice(9,9+compressedLength));
+		} else if (compressionOpcode == 1) { //bz2
+			data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
 			var header = "BZh1";
-			var bzData = new Uint8Array(4+data.length);
-			bzData[0]= 'B'.charCodeAt(0);
-			bzData[1]= 'Z'.charCodeAt(0);
-			bzData[2]= 'h'.charCodeAt(0);
-			bzData[3]= '1'.charCodeAt(0);
+			var bzData = new Uint8Array(4 + data.length);
+			bzData[0] = 'B'.charCodeAt(0);
+			bzData[1] = 'Z'.charCodeAt(0);
+			bzData[2] = 'h'.charCodeAt(0);
+			bzData[3] = '1'.charCodeAt(0);
 			bzData.set(data, 4)
 			decompressedData = bz2.decompress(bzData);
-		} else if(compressionOpcode == 2) { //gzip
-			data = new Uint8Array(dataview.buffer.slice(9,9+compressedLength));
+		} else if (compressionOpcode == 2) { //gzip
+			data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
 			decompressedData = new Uint8Array(gzip.unzip(data));
 		}
-		
+
 		return decompressedData;
 	}
 
-	readSector(buffer, pos, archiveId){
+	readSector(buffer, pos, archiveId) {
 		var convertedPos = pos * 520;
-		
+
 		let dataview = new DataView(this.datData.buffer);
 
 		var currentArchive;
@@ -70,15 +100,15 @@ export default class CacheRequester {
 			currentIndex = dataview.getUint8(convertedPos + 7);
 		}
 
-		var data; 
-		if(nextSector != 0 || buffer.byteLength == 512)
-			data = new Uint8Array(dataview.buffer.slice(convertedPos+8,convertedPos+520));
+		var data;
+		if (nextSector != 0 || buffer.byteLength == 512)
+			data = new Uint8Array(dataview.buffer.slice(convertedPos + 8, convertedPos + 520));
 		else
-			data = new Uint8Array(dataview.buffer.slice(convertedPos+8,convertedPos+8+(buffer.byteLength%512)));
-		
-		buffer.set(data, dataview.getInt16(convertedPos+2)*512);
+			data = new Uint8Array(dataview.buffer.slice(convertedPos + 8, convertedPos + 8 + (buffer.byteLength % 512)));
 
-		if(nextSector != 0)
+		buffer.set(data, dataview.getInt16(convertedPos + 2) * 512);
+
+		if (nextSector != 0)
 			this.readSector(buffer, nextSector);
 	}
 
