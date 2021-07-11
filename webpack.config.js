0fd5bddeb577e0502e44bd78776c46e2d67b21c5
JasonM97 <dezinated@gmail.com>
2021-07-10 21:36:18 -0400

    include worker loader into project and small html changes

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,19 @@ module.exports = {
     optimization: {
         minimize: false
     },
-    
+    module: {
+        rules: [
+            {
+                test: /\.worker\.js$/,
+                use: {
+                    loader: "worker-loader",
+                    options: {
+                        filename: '[name].[hash:8].js',
+                        // notice here
+                        inline: "fallback"
+                    }
+                },
+            },
+        ],
+    },
 };
