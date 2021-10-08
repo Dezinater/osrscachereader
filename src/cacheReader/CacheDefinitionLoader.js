f5e84ec567e02d5a6c635b9ec0918f16e4a3ddca
JasonM97 <dezinated@gmail.com>
2021-10-08 19:25:59 -0400

    cache now using promises

--- src/cacheReader/CacheDefinitionLoader.js
+++ src/cacheReader/CacheDefinitionLoader.js
@@ -2,26 +2,45 @@ import IndexType from './cacheTypes/IndexType.js'
 import ConfigType from './cacheTypes/ConfigType.js'
 
 export default class CacheDefinitionLoader {
-	constructor(indexId, archiveId, files){
+	constructor(indexId, archiveId, files) {
 		this.indexType = IndexType.valueOf(indexId);
 		this.archiveId = archiveId;
 		this.files = files;
 	}
 
-	load() {
-		var loader;
+	load(rscache) {
+		return new Promise((resolve, reject) => {
+			var loader;
 
-		if(this.indexType == IndexType.CONFIGS){
-			loader = new (ConfigType.valueOf(this.archiveId).loader)();
-		}else{
-			loader = new this.indexType.loader();
-		}
-		
-		for(var i=0;i<this.files.length;i++){
-			
-			//unload archive file memory to replace it with definition info
-			this.files[i].def = loader.load(this.files[i].content);
-			this.files[i].content = undefined;
-		}
+			if (this.indexType == IndexType.CONFIGS) {
+				loader = new (ConfigType.valueOf(this.archiveId).loader)();
+			} else {
+				loader = new this.indexType.loader();
+			}
+
+			var promises = [];
+			//console.log(this.files.length);
+			for (var i = 0; i < this.files.length; i++) {
+				var defId = this.files.length > 1 ? i : this.archiveId;
+				//unload archive file memory to replace it with definition info
+				//console.log(defId);
+				let loadPromise = Promise.resolve(loader.load(this.files[i].content, defId, rscache));
+
+				loadPromise.then((loadedDef) => {
+					//console.log(loadedDef.id);
+					if (this.files.length > 1) {
+						this.files[loadedDef.id].def = loadedDef;
+						this.files[loadedDef.id].content = undefined;
+					}else{
+						this.files[0].def = loadedDef;
+						this.files[0].content = undefined;
+					}
+				})
+				promises.push(loadPromise);
+				//this.files[i].def = loader.load(this.files[i].content, rscache);
+			}
+			Promise.all(promises).then(() => resolve());
+			//resolve();
+		});
 	}
 }
--- src/cacheReader/CacheRequester.js
+++ src/cacheReader/CacheRequester.js
@@ -1,13 +1,19 @@
 var gzip = require('gzip-js');
 import * as Ajax from './helpers/ajax.js'
 import { decompress } from 'bz2';
+import Worker from "./Worker.worker.js";
 
 
 export default class CacheRequester {
 	//should be used to make read requests from the cache
 	//this should make it easier to multithread/async this later on
 	constructor(rootDir) {
-		this.worker = new Worker(new URL('./worker.js', import.meta.url));
+		this.promises = {};
+		this.worker = new Worker();
+
+
+
+		//console.log(this.worker);
 		if ('caches' in window) { //if we are able to use cache api
 			var request = rootDir + "cache/" + "main_file_cache.dat2";
 			this.datDataPromise = caches.open('osrsrenderer').then((browserCache) => {
@@ -42,45 +48,85 @@ export default class CacheRequester {
 
 	}
 
+	readDataThreaded(index, size, segment, archiveId = 0) {
+		var promiseResolve;
+		var readPromise = new Promise((resolve, reject) => { promiseResolve = resolve; });
 
+		if (this.promises[index] == undefined)
+			this.promises[index] = {};
 
-	readData(index, size, segment, archiveId = 0) {
-		this.worker.postMessage("yeehaw");
-		this.worker.onmessage = function(event) {
-			//console.log(event);
-		};
+		if (this.promises[index][archiveId] == undefined) {
+			this.promises[index][archiveId] = [];
 
-		var compressedData = new Uint8Array(size);
+			var compressedData = new Uint8Array(size);
+			this.readSector(compressedData, segment, archiveId);
 
-		this.readSector(compressedData, segment, archiveId);
+			compressedData = compressedData.buffer;
+			//console.log(compressedData);
+			var localWorker = new Worker();
 
-		let dataview = new DataView(compressedData.buffer);
-		var compressionOpcode = dataview.getUint8(0);
-		var compressedLength = dataview.getUint32(1);
+			localWorker.postMessage({ index, segment, archiveId, compressedData }, [compressedData]);
+			//console.log(compressedData);
+			localWorker.onmessage = (event) => {
+				event.data.decompressedData = new Uint8Array(event.data.decompressedData);
 
-		var data;
-		var decompressedData;
-
-		if (compressionOpcode == 0) { //none
-			data = new Uint8Array(dataview.buffer.slice(5, 9 + compressedLength));
-			decompressedData = data;
-			index.revision = dataview.getUint16(data.buffer.byteLength)
-		} else if (compressionOpcode == 1) { //bz2
-			data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
-			var header = "BZh1";
-			var bzData = new Uint8Array(4 + data.length);
-			bzData[0] = 'B'.charCodeAt(0);
-			bzData[1] = 'Z'.charCodeAt(0);
-			bzData[2] = 'h'.charCodeAt(0);
-			bzData[3] = '1'.charCodeAt(0);
-			bzData.set(data, 4)
-			decompressedData = bz2.decompress(bzData);
-		} else if (compressionOpcode == 2) { //gzip
-			data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
-			decompressedData = new Uint8Array(gzip.unzip(data));
+				var length = this.promises[event.data.index.id][event.data.archiveId].length;
+				for (var i = 0; i < length; i++) {
+					this.promises[event.data.index.id][event.data.archiveId][i](event.data);
+				}
+				this.promises[event.data.index.id][event.data.archiveId] = undefined;
+				localWorker.terminate();
+				//delete localWorker;
+				localWorker = undefined;
+			};
 		}
 
-		return decompressedData;
+		this.promises[index][archiveId].push(promiseResolve);
+
+		return readPromise;
+	}
+
+	readData(index, size, segment, archiveId = 0) {
+
+		/*
+		this.worker.onmessage = function(event) {
+			//event.data.decompressedData
+			console.log(event);
+		};
+		*/
+
+		return Promise.resolve().then(() => {
+			var compressedData = new Uint8Array(size);
+			this.readSector(compressedData, segment, archiveId);
+
+			let dataview = new DataView(compressedData.buffer);
+			var compressionOpcode = dataview.getUint8(0);
+			var compressedLength = dataview.getUint32(1);
+
+			var data;
+			var decompressedData;
+
+			if (compressionOpcode == 0) { //none
+				data = new Uint8Array(dataview.buffer.slice(5, 9 + compressedLength));
+				decompressedData = data;
+				index.revision = dataview.getUint16(data.buffer.byteLength)
+			} else if (compressionOpcode == 1) { //bz2
+				data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
+				var header = "BZh1";
+				var bzData = new Uint8Array(4 + data.length);
+				bzData[0] = 'B'.charCodeAt(0);
+				bzData[1] = 'Z'.charCodeAt(0);
+				bzData[2] = 'h'.charCodeAt(0);
+				bzData[3] = '1'.charCodeAt(0);
+				bzData.set(data, 4)
+				decompressedData = bz2.decompress(bzData);
+			} else if (compressionOpcode == 2) { //gzip
+				data = new Uint8Array(dataview.buffer.slice(9, 9 + compressedLength));
+				decompressedData = new Uint8Array(gzip.unzip(data));
+			}
+
+			return { index, archiveId, decompressedData };
+		});
 	}
 
 	readSector(buffer, pos, archiveId) {
