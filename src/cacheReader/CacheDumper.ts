e15ab9b3838da889d121f40f07ff742bebd6593c
Dezinater <15971253+Dezinater@users.noreply.github.com>
2023-03-26 21:38:04 -0400

    dump configs and ignore huge files

--- src/cacheReader/CacheDumper.ts
+++ src/cacheReader/CacheDumper.ts
@@ -1,19 +1,23 @@
 
 import RSCache from "./RSCache";
 import IndexType from "./cacheTypes/IndexType.js"
+import ConfigType from "./cacheTypes/ConfigType.js"
 import { isBrowser } from "browser-or-node";
 import fs from "fs";
 
 export default class CacheDumper {
 
+    private ignoreList = ["FRAMES", "FRAMEMAPS", "MAPS", "MODELS"];
+
     private cache: RSCache;
     private outFolder: string;
+    private progressFunction: Function;
 
     private completedJobs = 0;
     private failedJobs = 0;
     private totalJobs = 0;
 
-    constructor(rscache, outFolder: string) {
+    constructor(rscache, outFolder: string, progressFunction: Function = () => { }) {
         if (isBrowser) {
             console.error("Run with Node to dump Cache files, web browser dumping currently not implemented");
             return;
@@ -24,6 +28,7 @@ export default class CacheDumper {
         }
         this.cache = rscache;
         this.outFolder = outFolder;
+        this.progressFunction = progressFunction;
     }
 
     outFolderCheck() {
@@ -31,38 +36,83 @@ export default class CacheDumper {
         fs.mkdirSync(this.outFolder);
     }
 
-    dumpAll() {
+    async dumpAll() {
         for (const [indexType, indexInfo] of Object.entries(IndexType)) {
+            if (this.ignoreList.includes(indexType)) {
+                continue;
+            }
+
             //@ts-ignore
             if (indexInfo.loader != undefined) {
-                //@ts-ignore
-                this.dumpIndex(indexInfo.id);
-                return;
+                try {
+                    await this.dumpIndex(indexInfo, indexType);
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
+
+        for (const [configType, configInfo] of Object.entries(ConfigType)) {
+            if (this.ignoreList.includes(configType)) {
+                continue;
+            }
+
+            //@ts-ignore
+            if (configInfo.loader != undefined) {
+                try {
+                    await this.dumpConfig(configInfo, configType);
+                } catch (e) {
+                    console.log(e);
+                }
             }
         }
     }
 
-    async dumpIndex(indexId) {
-        this.outFolderCheck();
-        const archivesCount = this.cache.indicies[indexId].archivesCount;
+    updateProgress(jobSuccess: boolean) {
+        if (jobSuccess) {
+            this.completedJobs++;
+        } else {
+            this.failedJobs++;
+        }
 
-        for (let archiveId = 0; archiveId < archivesCount; archiveId++) {
-            this.totalJobs++;
-            try {
-                let files = await this.cache.getAllFiles(indexId, archiveId);
-                fs.mkdir(this.outFolder + archiveId, () => {
-                    files.forEach(file => {
-                        fs.writeFile(this.outFolder + archiveId + "/" + file.def.id + ".json", JSON.stringify(file.def, null, 2), () => {
-                            this.completedJobs++;
-                        });
+        const total = (this.failedJobs + this.completedJobs) / this.totalJobs;
+        const errorRate = this.failedJobs / (this.failedJobs + this.completedJobs);
+        this.progressFunction(total, errorRate);
+    }
+
+    async dumpConfig(configInfo, name) {
+        if (!fs.existsSync(this.outFolder + "CONFIGS")) fs.mkdirSync(this.outFolder + "CONFIGS");
+        if (!fs.existsSync(this.outFolder + "CONFIGS/" + name)) fs.mkdirSync(this.outFolder + "CONFIGS/" + name);
+        let files = await this.cache.getAllFiles(IndexType.CONFIGS.id, configInfo.id);
+        files.forEach(file => {
+            fs.writeFileSync(this.outFolder + "CONFIGS/" + name + "/" + file.def.id + ".json", JSON.stringify(file.def, null, 2));
+        });
+    }
+
+    async dumpIndex(indexInfo, name) {
+        this.outFolderCheck();
+        const archivesCount = this.cache.indicies[indexInfo.id].archivesCount;
+        if (!fs.existsSync(this.outFolder + name)) fs.mkdirSync(this.outFolder + name);
+
+        this.dumpArchives(indexInfo, archivesCount)
+        this.totalJobs += archivesCount;
+    }
+
+    async dumpArchives(indexInfo, archiveId) {
+        try {
+            let files = await this.cache.getAllFiles(indexInfo.id, archiveId);
+            fs.mkdir(this.outFolder + name + "/" + archiveId, () => {
+                files.forEach(file => {
+                    fs.writeFile(this.outFolder + name + "/" + archiveId + "/" + file.def.id + ".json", JSON.stringify(file.def, null, 2), () => {
+                        this.updateProgress(true);
                     });
                 });
-            } catch (e) {
-                this.failedJobs++;
-                console.warn(`Error Loading { Index: ${indexId} Archive: ${archiveId} }`);
-                console.log(e);
-            }
+            });
+        } catch (e) {
+            this.updateProgress(false);
+            console.warn(`Error Loading { Index: ${indexInfo.id} Archive: ${archiveId} }`);
         }
     }
+
 
 }
