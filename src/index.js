b16ff04cab031f0fac8e1455b5eeee7b6efd5196
Dezinater <15971253+Dezinater@users.noreply.github.com>
2023-07-03 17:23:34 -0400

    export gltfexporter

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import RSCache from './cacheReader/RSCache.js'
 import IndexType from './cacheReader/cacheTypes/IndexType.js'
 import ConfigType from './cacheReader/cacheTypes/ConfigType.js'
 import Matrix from './cacheReader/cacheTypes/anim/MatrixTest.js';
+import GLTFExporter from './cacheReader/exporters/GLTFExporter.js';
 
-export { RSCache, IndexType, ConfigType, Matrix };
+
+export { RSCache, IndexType, ConfigType, Matrix, GLTFExporter };
