02d77d8b010fbcdc51fe3e750431c570b595b68a
Dezinater <15971253+Dezinater@users.noreply.github.com>
2023-09-10 13:37:01 -0400

    export cache dumper

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import IndexType from './cacheReader/cacheTypes/IndexType.js'
 import ConfigType from './cacheReader/cacheTypes/ConfigType.js'
 import Matrix from './cacheReader/cacheTypes/anim/Matrix.js';
 import GLTFExporter from './cacheReader/exporters/GLTFExporter.js';
+import CacheDumper from './cacheReader/CacheDumper.js'
 
-
-export { RSCache, IndexType, ConfigType, Matrix, GLTFExporter };
+export { RSCache, IndexType, ConfigType, Matrix, GLTFExporter, CacheDumper };
