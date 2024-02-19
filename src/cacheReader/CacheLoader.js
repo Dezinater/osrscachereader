dc9f00c85e6cf88d855212457510373bd2f00f5c
Dezinater <15971253+Dezinater@users.noreply.github.com>
2024-02-19 13:10:40 -0500

    add openrs2 cache loading by version or timestamp

--- src/cacheReader/CacheLoader.js
+++ src/cacheReader/CacheLoader.js
@@ -16,12 +16,26 @@ export default class CacheLoader {
         xteas: undefined,
     };
 
+    //cache promise is complete after all promises are setup
+    cachePromise;
+    cachePromiseResolve;
+
     constructor(path, onDownloadProgress) {
         this.onDownloadProgress = onDownloadProgress;
+        this.cachePromise = new Promise((resolve, reject) => {
+            this.cachePromiseResolve = resolve;
+        });
 
-        if (this.isValidHttpUrl(path) || isBrowser) {
+        if (path == "latest" || path == undefined) {    //load by current date
+            this.loadByTimestamp(new Date());
+        } else if (this.isValidHttpUrl(path)) {         //load by url
             this.fetchURL(path);
-        } else {
+        } else if (!isNaN(path)) {                      //load by version number
+            let version = Number(path);
+            this.loadByVersion(version);
+        } else if (path.prototype.name == "Date") {     //load by date
+            this.loadByTimestamp(path);
+        } else {                                        //load by local file path
             this.loadFile(path);
         }
 
@@ -29,6 +43,7 @@ export default class CacheLoader {
 
     getResults() {
         return new Promise(async resolve => {
+            await this.cachePromise;
             const datPromiseResults = await this.promises.datFile;
             const indexPromiseResults = await Promise.all(this.promises.indexFiles);
             const xteasResults = await this.promises.xteas;
@@ -55,9 +70,26 @@ export default class CacheLoader {
         const zip = zipBufferPromise.then(zip => unzipSync(new Uint8Array(zip)));
         this.promises.datFile = zip.then(dir => dir['cache/' + this.datFile]);
         this.promises.indexFiles = this.indexFiles.map((indexFile, i) => {
-            return zip.then(dir => dir['cache/' + indexFile]).catch(_ => { console.warn(`${IndexType.keyOf(i)} (Index ${i}) will not load without ${indexFile}`)});
+            return zip.then(dir => dir['cache/' + indexFile]).catch(_ => { console.warn(`${IndexType.keyOf(i)} (Index ${i}) will not load without ${indexFile}`) });
         });
         this.promises.xteas = zip.then(dir => dir['cache/xteas.json']).catch(e => { console.warn("Maps (Index 5) will not load without xteas.json") });
+    }
+
+    loadByVersion(version) {
+        axios.get("https://archive.openrs2.org/caches.json", { responseType: 'json' }).then(caches => {
+            let filtered = caches.data.filter(x => x.game == "oldschool" && x.builds.length > 0 && x.builds[0].major == version);
+            let sorted = filtered.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+            this.fetchURL(`https://archive.openrs2.org/caches/runescape/${sorted[0].id}/disk.zip`);
+        });
+    }
+
+    loadByTimestamp(timestamp) {
+        axios.get("https://archive.openrs2.org/caches.json", { responseType: 'json' }).then(caches => {
+            let filtered = caches.data.filter(x => x.game == "oldschool");
+            filtered.forEach(x => x.score = Math.abs(new Date(x.timestamp) - timestamp)); //rank them by distance
+            let sorted = filtered.sort((a, b) => a.score - b.score);
+            this.fetchURL(`https://archive.openrs2.org/caches/runescape/${sorted[0].id}/disk.zip`);
+        });
     }
 
     fetchURL(url) {
@@ -70,11 +102,13 @@ export default class CacheLoader {
 
             this.promises.datFile = axios.get(url + this.datFile, { onDownloadProgress: this.onDownloadProgress, responseType: 'arraybuffer', }).then(x => new Uint8Array(x.data));
             this.indexFiles.forEach((indexFile, i) => {
-                let indexPromise = axios.get(url + indexFile, { responseType: 'arraybuffer' }).then(x => new Uint8Array(x.data)).catch(_ => { console.warn(`${IndexType.keyOf(i)} (Index ${i}) will not load without ${indexFile}`)});
+                let indexPromise = axios.get(url + indexFile, { responseType: 'arraybuffer' }).then(x => new Uint8Array(x.data)).catch(_ => { console.warn(`${IndexType.keyOf(i)} (Index ${i}) will not load without ${indexFile}`) });
                 this.promises.indexFiles.push(indexPromise);
             });
             this.promises.xteas = axios.get(url + "xteas.json", { responseType: 'json', }).then(x => this.readXteas(x.data)).catch(e => { console.warn("Maps (Index 5) will not load without xteas.json") });
         }
+
+        this.cachePromiseResolve();
     }
 
     loadFile(path) {
@@ -104,6 +138,8 @@ export default class CacheLoader {
                 resolve(this.readXteas(data));
             }));
         }
+
+        this.cachePromiseResolve();
     }
 
     readXteas(xteasData) {
