5ed172b3ac12c06c7b3c3ded5dc561b54534377c
Dezinater <15971253+Dezinater@users.noreply.github.com>
2024-02-20 14:33:03 -0500

    update and clean dev testing code

--- src/index-dev-node.js
+++ src/index-dev-node.js
@@ -5,4 +5,5 @@ import ConfigType from './cacheReader/cacheTypes/ConfigType.js'
 let cache = new RSCache("./cache/");
 cache.onload.then(async () => {
     console.log(await cache.getNPC(2042))
+    cache.close();
 });
