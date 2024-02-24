7e8371c5fb1d08e3230f01f6aa4671bac735df10
Dezinater <15971253+Dezinater@users.noreply.github.com>
2024-02-24 13:57:15 -0500

    updated loader for newest cache and added method to convert obtain model

--- src/cacheReader/loaders/ObjectLoader.js
+++ src/cacheReader/loaders/ObjectLoader.js
@@ -1,3 +1,7 @@
+import { ModelDefinition } from "./ModelLoader.js";
+import ModelGroup from "../helpers/ModelGroup.js";
+import IndexType from "../cacheTypes/IndexType.js";
+
 /**
 * @class ObjectDefinition
 * @category Definitions
@@ -21,7 +25,7 @@ export class ObjectDefinition {
 	 * @type {Array} 
 	 */
 	objectModels = [];
-	
+
 	/**
 	 * Name of the Object
 	 * @type {string} 
@@ -78,7 +82,7 @@ export class ObjectDefinition {
 
 	/** @type {number} */
 	decorDisplacement = 16;
-	
+
 	/**
 	 * Number from 0 to 255. Overrides Object model's ambient lighting 
 	 * @type {Byte} 
@@ -123,7 +127,7 @@ export class ObjectDefinition {
 
 	/** @type {boolean} */
 	rotated = false;
-	
+
 	/** @type {boolean} */
 	shadow = true;
 
@@ -162,10 +166,10 @@ export class ObjectDefinition {
 
 	/** @type {boolean} */
 	obstructsGround = false;
-	
+
 	/** @type {number} */
 	hollow = false;
-	
+
 	/** @type {number} */
 	supportsItems = -1;
 
@@ -174,41 +178,151 @@ export class ObjectDefinition {
 
 	/** @type {number} */
 	varpID = -1;
-	
+
 	/** @type {Array} */
 	configChangeDest = [];
-	
+
 	/** @type {number} */
 	ambientSoundId = -1;
-	
+
 	/** @type {number} */
 	ambientSoundDistance = 0;
-	
+
 	/** @type {number} */
 	ambientSoundChangeTicksMin = 0;
-	
+
 	/** @type {number} */
 	ambientSoundChangeTicksMax = 0;
-	
+
 	/** @type {Array<number>} */
 	ambientSoundIds = [];
-	
+
+	/** @type {number} */
+	ambientSoundRetain = 0;
+
 	/** @type {number} */
 	mapAreaId = -1;
-	
+
 	/** @type {boolean} */
 	randomizeAnimStart;
-	
+
 	/** @type {Object} */
 	params;
 
 	constructor() {
 	}
+
+	async getModel(cache, modelType, rotation) {
+		let modelData = null;
+		let isRotated;
+		if (this.objectTypes == null) { // if it has no types then merge
+			if (modelType != 10) {
+				return null;
+			}
+
+			if (this.objectModels == null) {
+				return null;
+			}
+
+			isRotated = this.rotated;
+			if (modelType == 2 && rotation > 3) {
+				isRotated = !isRotated;
+			}
+
+			let models = [];
+			for (let i = 0; i < this.objectModels.length; ++i) {
+				modelData = await cache.getDef(IndexType.MODELS.id, this.objectModels[i]);
+
+				if (this.objectModels.length > 1) {
+					models[i] = modelData;
+				}
+			}
+
+			if (this.objectModels.length > 1) {
+				modelData = new ModelGroup(models).getMergedModel();
+			}
+		} else {
+			let var9 = -1;
+
+			for (let i = 0; i < this.objectTypes.length; ++i) {
+				if (this.objectTypes[i] == modelType) {
+					var9 = i;
+					break;
+				}
+			}
+
+			if (var9 == -1) {
+				return null;
+			}
+
+			let modelId = this.objectModels[var9];
+			let var10 = this.rotated ^ rotation > 3;
+
+			modelData = await cache.getDef(IndexType.MODELS.id, modelId);
+			if (var10) {
+				modelData.method1194();
+			}
+		}
+
+		if (this.modelSizeX == 128 && this.modelSizeHeight == 128 && this.modelSizeY == 128) {
+			isRotated = false;
+		} else {
+			isRotated = true;
+		}
+
+		let var11;
+		if (this.offsetX == 0 && this.offsetHeight == 0 && this.offsetY == 0) {
+			var11 = false;
+		} else {
+			var11 = true;
+		}
+
+		if (modelType == 4 && rotation > 3) {
+			modelData.method1206(256);
+			modelData.changeOffset(45, 0, -45);
+		}
+
+		rotation &= 3;
+		if (rotation == 1) {
+			modelData.method1188();
+		} else if (rotation == 2) {
+			modelData.method1190();
+		} else if (rotation == 3) {
+			modelData.method1189();
+		}
+
+		if (this.recolorFrom != null) {
+			for (let var7 = 0; var7 < this.recolorFrom.length; ++var7) {
+				modelData.recolor(this.recolorFrom[var7], this.recolorTo[var7]);
+			}
+		}
+
+		if (this.retextureFrom != null) {
+			for (let var7 = 0; var7 < this.retextureFrom.length; ++var7) {
+				modelData.retexture(this.retextureFrom[var7], this.retextureTo[var7]);
+			}
+		}
+
+		if (isRotated) {
+			modelData.resize(this.modelSizeX, this.modelSizeHeight, this.modelSizeY);
+		}
+
+		if (var11) {
+			modelData.changeOffset(this.offsetX, this.offsetHeight, this.offsetY);
+		}
+
+		return modelData;
+	}
+
+
 }
 export default class ObjectLoader {
+	rev220SoundData = true;
+	configureForRevision(revision) {
+		this.rev220SoundData = revision >= 1673;
+	}
 
 	load(bytes, id) {
-		//console.log(id, bytes.length);
 		let def = new ObjectDefinition();
 		def.id = id;
 		let dataview = new DataView(bytes.buffer);
@@ -335,7 +449,7 @@ export default class ObjectLoader {
 		else if (opcode == 62) {
 			def.rotated = true;
 		}
-		else if (opcode == 64) { 
+		else if (opcode == 64) {
 			def.shadow = false;
 		}
 		else if (opcode == 65) {
@@ -398,11 +512,18 @@ export default class ObjectLoader {
 		else if (opcode == 78) {
 			def.ambientSoundId = dataview.readUint16();
 			def.ambientSoundDistance = dataview.readUint8();
+			if (this.rev220SoundData) {
+				def.ambientSoundRetain = dataview.readUint8();
+			}
 		}
 		else if (opcode == 79) {
 			def.ambientSoundChangeTicksMin = dataview.readUint16();
 			def.ambientSoundChangeTicksMax = dataview.readUint16();
 			def.ambientSoundDistance = dataview.readUint8();
+			if (this.rev220SoundData) {
+				def.ambientSoundRetain = dataview.readUint8();
+			}
+
 			var length = dataview.readUint8();
 			let ambientSoundIds = [];
 
