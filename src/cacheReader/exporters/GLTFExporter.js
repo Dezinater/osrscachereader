ab2db7faaf429796546167a6cf160a804c9cd6f4
supalosa <supalosa@supalosa.com>
2024-04-08 01:10:04 +1000

    duplicate faces (warning, large models)

--- src/cacheReader/exporters/GLTFExporter.js
+++ src/cacheReader/exporters/GLTFExporter.js
@@ -133,21 +133,21 @@ function adjustForBrightness(rgb, brightness) {
 class GLTFFile {
     //Basically just setting it up for 1 model
     scene = 0;
-    scenes = [
-        { nodes: [0] }
-    ];
+    scenes = [{ nodes: [0] }];
 
     nodes = [{ mesh: 0 }];
 
     meshes = [
         {
-            primitives: [{
-                attributes: {
-                    POSITION: 1
+            primitives: [
+                {
+                    attributes: {
+                        POSITION: 0,
+                    },
+                    //indices: 0,
                 },
-                indices: 0,
-            }]
-        }
+            ],
+        },
     ];
 
     textures = [];
@@ -162,14 +162,18 @@ class GLTFFile {
     accessors = [];
 
     asset = {
-        version: "2.0"
+        version: "2.0",
     };
 
     addIndicies(indicies) {
-        let indicesBytes = new Uint8Array(new Uint16Array(indicies.flat()).buffer)
+        let indicesBytes = new Uint8Array(
+            new Uint16Array(indicies.flat()).buffer
+        );
         let buffersAmount = this.buffers.length;
         this.buffers.push({
-            uri: "data:application/octet-stream;base64," + base64.bytesToBase64(indicesBytes),
+            uri:
+                "data:application/octet-stream;base64," +
+                base64.bytesToBase64(indicesBytes),
             byteLength: indicesBytes.length,
         });
 
@@ -177,10 +181,12 @@ class GLTFFile {
             buffer: buffersAmount,
             byteOffset: 0,
             byteLength: indicesBytes.length,
-            target: 34963
+            target: 34963,
         });
 
-        let max = indicies.flat().reduce((max, current) => Math.max(max, current));
+        let max = indicies
+            .flat()
+            .reduce((max, current) => Math.max(max, current));
         this.accessors.push({
             bufferView: 0,
             byteOffset: 0,
@@ -188,7 +194,7 @@ class GLTFFile {
             count: indicesBytes.length / 2,
             type: "SCALAR",
             max: [max],
-            min: [0]
+            min: [0],
         });
     }
 
@@ -196,16 +202,27 @@ class GLTFFile {
         let max = [0, 0, 0];
         let min = [0, 0, 0];
         for (let i = 0; i < verticies.length; i++) {
-            max = [Math.max(max[0], verticies[i][0]), Math.max(max[1], verticies[i][1]), Math.max(max[2], verticies[i][2])];
-            min = [Math.min(min[0], verticies[i][0]), Math.min(min[1], verticies[i][1]), Math.min(min[2], verticies[i][2])];
+            max = [
+                Math.max(max[0], verticies[i][0]),
+                Math.max(max[1], verticies[i][1]),
+                Math.max(max[2], verticies[i][2]),
+            ];
+            min = [
+                Math.min(min[0], verticies[i][0]),
+                Math.min(min[1], verticies[i][1]),
+                Math.min(min[2], verticies[i][2]),
+            ];
         }
 
-        let verticiesBytes = new Uint8Array(new Float32Array(verticies.flat()).buffer)
-
+        let verticiesBytes = new Uint8Array(
+            new Float32Array(verticies.flat()).buffer
+        );
 
         let buffersAmount = this.buffers.length;
         this.buffers.push({
-            uri: "data:application/octet-stream;base64," + base64.bytesToBase64(verticiesBytes),
+            uri:
+                "data:application/octet-stream;base64," +
+                base64.bytesToBase64(verticiesBytes),
             byteLength: verticiesBytes.length,
         });
 
@@ -222,20 +239,29 @@ class GLTFFile {
             count: verticies.length,
             type: "VEC3",
             max,
-            min
+            min,
         });
     }
 
     addMorphTarget(verticies) {
-        //verticies.forEach(x => x[2] *= -1);
         let max = [verticies[0][0], verticies[0][1], verticies[0][2]];
         let min = [verticies[0][0], verticies[0][1], verticies[0][2]];
         for (let i = 0; i < verticies.length; i++) {
-            max = [Math.max(max[0], verticies[i][0]), Math.max(max[1], verticies[i][1]), Math.max(max[2], verticies[i][2])];
-            min = [Math.min(min[0], verticies[i][0]), Math.min(min[1], verticies[i][1]), Math.min(min[2], verticies[i][2])];
+            max = [
+                Math.max(max[0], verticies[i][0]),
+                Math.max(max[1], verticies[i][1]),
+                Math.max(max[2], verticies[i][2]),
+            ];
+            min = [
+                Math.min(min[0], verticies[i][0]),
+                Math.min(min[1], verticies[i][1]),
+                Math.min(min[2], verticies[i][2]),
+            ];
         }
 
-        let verticiesBytes = new Uint8Array(new Float32Array(verticies.flat()).buffer)
+        let verticiesBytes = new Uint8Array(
+            new Float32Array(verticies.flat()).buffer
+        );
 
         if (!("targets" in this.meshes[0].primitives[0])) {
             this.meshes[0].primitives[0].targets = [];
@@ -249,17 +275,19 @@ class GLTFFile {
         } else {
             this.meshes[0].weights.push(0);
         }
-        this.meshes[0].primitives[0].targets.push({ "POSITION": buffersAmount });
+        this.meshes[0].primitives[0].targets.push({ POSITION: buffersAmount });
 
         this.buffers.push({
-            uri: "data:application/octet-stream;base64," + base64.bytesToBase64(verticiesBytes),
+            uri:
+                "data:application/octet-stream;base64," +
+                base64.bytesToBase64(verticiesBytes),
             byteLength: verticiesBytes.length,
         });
         this.bufferViews.push({
             buffer: buffersAmount,
             byteOffset: 0,
             byteLength: verticiesBytes.length,
-            target: 34962
+            target: 34962,
         });
 
         this.accessors.push({
@@ -269,29 +297,35 @@ class GLTFFile {
             count: verticies.length,
             type: "VEC3",
             max,
-            min
+            min,
         });
 
         //animations needs to be adjusted if more morph targets are added after anims
     }
 
     addAnimation(targets, lengths, morphTargetsAmount) {
-        targets = targets.map(x => {
-            let oneHot = new Array(morphTargetsAmount).fill(0)
+        targets = targets.map((x) => {
+            let oneHot = new Array(morphTargetsAmount).fill(0);
             oneHot[x] = 1;
-            return oneHot
+            return oneHot;
         }); //one hot encoding
 
-        let targetBytes = new Uint8Array(new Float32Array(targets.flat()).buffer);
+        let targetBytes = new Uint8Array(
+            new Float32Array(targets.flat()).buffer
+        );
         let lengthsBytes = new Uint8Array(new Float32Array(lengths).buffer);
 
-        let mergedBytes = new Uint8Array(lengthsBytes.length + targetBytes.length);
+        let mergedBytes = new Uint8Array(
+            lengthsBytes.length + targetBytes.length
+        );
         mergedBytes.set(lengthsBytes);
         mergedBytes.set(targetBytes, lengthsBytes.length);
 
         let buffersAmount = this.buffers.length;
         this.buffers.push({
-            uri: "data:application/octet-stream;base64," + base64.bytesToBase64(mergedBytes),
+            uri:
+                "data:application/octet-stream;base64," +
+                base64.bytesToBase64(mergedBytes),
             byteLength: mergedBytes.length,
         });
 
@@ -322,7 +356,7 @@ class GLTFFile {
             count: targets.length,
             type: "SCALAR",
             max: [max],
-            min: [min]
+            min: [min],
         });
 
         this.accessors.push({
@@ -332,23 +366,27 @@ class GLTFFile {
             count: morphTargetsAmount * targets.length,
             type: "SCALAR",
             max: [1.0],
-            min: [0.0]
+            min: [0.0],
         });
 
         let animationsLength = this.animations.length;
         this.animations.push({
-            samplers: [{
-                input: accessorsLength,
-                interpolation: "STEP",
-                output: accessorsLength + 1
-            }],
-            channels: [{
-                sampler: 0,
-                target: {
-                    node: 0,
-                    path: "weights"
-                }
-            }]
+            samplers: [
+                {
+                    input: accessorsLength,
+                    interpolation: "STEP",
+                    output: accessorsLength + 1,
+                },
+            ],
+            channels: [
+                {
+                    sampler: 0,
+                    target: {
+                        node: 0,
+                        path: "weights",
+                    },
+                },
+            ],
         });
     }
 
@@ -358,41 +396,43 @@ class GLTFFile {
         const texturesAmount = this.textures.length;
         this.textures.push({
             source: texturesAmount,
-            sampler: texturesAmount
+            sampler: texturesAmount,
         });
 
         this.images.push({
-            uri: colorPalettePng
-		});
+            uri: colorPalettePng,
+        });
 
         this.samplers.push({
             magFilter: 9728,
             minFilter: 9987,
             wrapS: 33648,
-            wrapT: 33648
+            wrapT: 33648,
         });
 
         this.materials.push({
-            pbrMetallicRoughness : {
-			  baseColorFactor: [ 1.0, 1.0, 1.0, 1.0],
-              baseColorTexture : {
-                index : 0
-              },
-              metallicFactor : 0.0,
-              roughnessFactor : 1.0
-            }
+            pbrMetallicRoughness: {
+                baseColorFactor: [1.0, 1.0, 1.0, 1.0],
+                baseColorTexture: {
+                    index: 0,
+                },
+                metallicFactor: 0.0,
+                roughnessFactor: 1.0,
+            },
         });
-        
+
         let buffersAmount = this.buffers.length;
         this.buffers.push({
-            uri: "data:application/gltf-buffer;base64," + base64.bytesToBase64(uvBytes),
+            uri:
+                "data:application/gltf-buffer;base64," +
+                base64.bytesToBase64(uvBytes),
             byteLength: uvBytes.length,
         });
 
         this.bufferViews.push({
             buffer: buffersAmount,
             byteOffset: 0,
-            target : 34962,
+            target: 34962,
             byteLength: uvBytes.length,
         });
 
@@ -412,109 +452,124 @@ class GLTFFile {
             min,
         });
 
-        this.meshes[0].primitives[0].attributes.TEXCOORD_0 = this.accessors.length - 1;
+        this.meshes[0].primitives[0].attributes.TEXCOORD_0 =
+            this.accessors.length - 1;
         this.meshes[0].primitives[0].material = 0;
     }
 }
 
 export default class GLTFExporter {
-
     verticies = [];
     morphTargetsAmount = 0;
+
+    remappedVertices = {};
 
     constructor(def) {
         this.file = new GLTFFile();
 
         this.verticies = [];
-        for (let i = 0; i < def.vertexPositionsX.length; i++) {
-            this.verticies.push([def.vertexPositionsX[i], -def.vertexPositionsY[i], -def.vertexPositionsZ[i]]);
-        }
 
-        let indicies = [];
+        // n.b. we reorder the vertices by faces. this duplicates all of the vertices but allows us to
+        // apply per-face textures/colours and removes the need for indices.
         for (let i = 0; i < def.faceVertexIndices1.length; i++) {
-            indicies.push([def.faceVertexIndices1[i], def.faceVertexIndices2[i], def.faceVertexIndices3[i]]);
+            const v1 = def.faceVertexIndices1[i];
+            const v2 = def.faceVertexIndices2[i];
+            const v3 = def.faceVertexIndices3[i];
+            const faceVertices = [v1, v2, v3];
+            faceVertices.forEach((idx, pos) => {
+                this.verticies.push([
+                    def.vertexPositionsX[idx],
+                    -def.vertexPositionsY[idx],
+                    -def.vertexPositionsZ[idx],
+                ]);
+                if (!(idx in this.remappedVertices)) {
+                    this.remappedVertices[idx] = [];
+                }
+                // maintain a multimap of original vertex ID to where they are now in `this.verticies`.
+                this.remappedVertices[idx].push(i * 3 + pos);
+            });
         }
-
-        this.file.addIndicies(indicies);
         this.file.addVerticies(this.verticies);
     }
 
     addMorphTarget(morphVertices) {
+        let newMorphVertices = [];
         for (let i = 0; i < morphVertices.length; i++) {
-            morphVertices[i][0] -= this.verticies[i][0];
-            morphVertices[i][1] -= this.verticies[i][1];
-            morphVertices[i][2] -= this.verticies[i][2];
+            const realVertices = this.remappedVertices[i];
+            for (const realVertex of realVertices) {
+                newMorphVertices[realVertex] = [];
+                newMorphVertices[realVertex][0] =
+                    morphVertices[i][0] - this.verticies[realVertex][0];
+                newMorphVertices[realVertex][1] =
+                    morphVertices[i][1] - this.verticies[realVertex][1];
+                newMorphVertices[realVertex][2] =
+                    morphVertices[i][2] - this.verticies[realVertex][2];
+            }
         }
 
         this.morphTargetsAmount++;
-        this.file.addMorphTarget(morphVertices);
+        this.file.addMorphTarget(newMorphVertices);
     }
 
     addAnimation(def) {
-        let frames = def.frameIDs.map(x => x & 65535);
+        let frames = def.frameIDs.map((x) => x & 65535);
         let lengths = Object.assign([], def.frameLengths);
         for (let i = 1; i < lengths.length; i++) {
             lengths[i] += lengths[i - 1];
         }
-        lengths = lengths.map(x => x / 50);
+        lengths = lengths.map((x) => x / 50);
         this.file.addAnimation(frames, lengths, this.morphTargetsAmount);
     }
 
     addColors(def) {
-		const faceColors = {};
-		const colorToPaletteIndex = {};
-		const order = [];
+        const faceColors = {};
+        const colorToPaletteIndex = {};
+        const order = [];
 
-		for (let i = 0; i < def.faceColors.length; ++i) {
-			if (faceColors[def.faceColors[i]]) {
-				continue;
-			}
-			let rscolor = def.faceColors[i];
-			let color = HSLtoRGB(rscolor, BRIGHTNESS_MAX);
-			let r = ((color >> 16) & 0xff) / 255.0;
-			let g = ((color >> 8) & 0xff) / 255.0;
-			let b = (color & 0xff) / 255.0;
-			console.log(`rscolor ${rscolor} rgb ${r} ${g} ${b}`)
-			faceColors[rscolor] = color;
-			colorToPaletteIndex[rscolor] = Object.keys(faceColors).length - 1;
-			order.push(rscolor);
-		}
-		const numUniqueColors = Object.keys(faceColors).length;
-		// create a texture for the face colors
-		const pSize = 16;
-		const canvas = createCanvas(numUniqueColors * pSize, pSize, "png");
-		const ctx = canvas.getContext('2d');
-		let xx = 0;
-		for (const rscolor of order) {
-			const rgb = faceColors[rscolor];
-			let r = ((rgb >> 16) & 0xff);
-			let g = ((rgb >> 8) & 0xff);
-			let b = (rgb & 0xff);
-			ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-			ctx.fillRect(xx, 0, pSize, pSize);
-			xx += pSize;
-		}
-		const colorPalettePng = canvas.toDataURL();
+        for (let i = 0; i < def.faceColors.length; ++i) {
+            if (faceColors[def.faceColors[i]]) {
+                continue;
+            }
+            let rscolor = def.faceColors[i];
+            let color = HSLtoRGB(rscolor, BRIGHTNESS_MAX);
+            let r = ((color >> 16) & 0xff) / 255.0;
+            let g = ((color >> 8) & 0xff) / 255.0;
+            let b = (color & 0xff) / 255.0;
+            console.log(`rscolor ${rscolor} rgb ${r} ${g} ${b}`);
+            faceColors[rscolor] = color;
+            colorToPaletteIndex[rscolor] = Object.keys(faceColors).length - 1;
+            order.push(rscolor);
+        }
+        const numUniqueColors = Object.keys(faceColors).length;
+        // create a texture for the face colors
+        const pSize = 16;
+        const canvas = createCanvas(numUniqueColors * pSize, pSize, "png");
+        const ctx = canvas.getContext("2d");
+        let xx = 0;
+        for (const rscolor of order) {
+            const rgb = faceColors[rscolor];
+            let r = (rgb >> 16) & 0xff;
+            let g = (rgb >> 8) & 0xff;
+            let b = rgb & 0xff;
+            ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+            ctx.fillRect(xx, 0, pSize, pSize);
+            xx += pSize;
+        }
+        const colorPalettePng = canvas.toDataURL();
 
-		// TODO: this is not right - we need to duplicate vertex positions
-        let uvs = new Array(def.vertexPositionsX.length);
-		const half = (1 / numUniqueColors) / 2;
-		const full = 1 / numUniqueColors;
-		for (let i = 0; i < def.faceColors.length; ++i) {
-			const faceColor = def.faceColors[i];
-			const v1 = def.faceVertexIndices1[i];
-			const v2 = def.faceVertexIndices2[i];
-			const v3 = def.faceVertexIndices3[i];
-			const paletteIndex = colorToPaletteIndex[faceColor];
-			uvs[v1] = [paletteIndex / numUniqueColors + half, 0.33];
-			uvs[v2] = [paletteIndex / numUniqueColors + half, 0.5];
-			uvs[v3] = [paletteIndex / numUniqueColors + half, 0.66];
-		}
+        let uvs = new Array(def.faceColors.length * 3);
+        const half = 1 / numUniqueColors / 2;
+        for (let i = 0; i < def.faceColors.length; ++i) {
+            const faceColor = def.faceColors[i];
+            const paletteIndex = colorToPaletteIndex[faceColor];
+            uvs[i * 3] = [paletteIndex / numUniqueColors + half, 0.33];
+            uvs[i * 3 + 1] = [paletteIndex / numUniqueColors + half, 0.5];
+            uvs[i * 3 + 2] = [paletteIndex / numUniqueColors + half, 0.66];
+        }
         this.file.addColors(uvs, colorPalettePng, colorToPaletteIndex);
     }
 
     export() {
         return JSON.stringify(this.file);
     }
-
 }
