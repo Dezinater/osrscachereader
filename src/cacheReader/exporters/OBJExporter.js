5bf7a5548858b2f910353f270d985d3c9aba68fa
Marshall Bowers <git@maxdeviant.com>
2025-02-16 16:21:48 -0500

    Don't use commas when writing vertices to OBJ files

--- src/cacheReader/exporters/OBJExporter.js
+++ src/cacheReader/exporters/OBJExporter.js
@@ -12,7 +12,7 @@ export default class OBJExporter {
             indicies.push([def.faceVertexIndices1[i], def.faceVertexIndices2[i], def.faceVertexIndices3[i]]);
         }
 
-        let verticiesString = verticies.map((x) => "v " + x.join(", ")).join("\n");
+        let verticiesString = verticies.map((x) => "v " + x.join(" ")).join("\n");
         let indiciesString = indicies.map((x) => "f " + x.map((y) => y + 1).join(" ")).join("\n");
         return verticiesString + "\n" + indiciesString;
     }
