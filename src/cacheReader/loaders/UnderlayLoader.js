ad5cccaff9fec35ecf7cc64c19e1500bc7359536
Dezinater <15971253+Dezinater@users.noreply.github.com>
2025-08-14 22:13:47 -0400

    update underlayloader

--- src/cacheReader/loaders/UnderlayLoader.js
+++ src/cacheReader/loaders/UnderlayLoader.js
@@ -7,6 +7,9 @@
 export class UnderlayDefinition {
     id;
     color;
+    hue;
+    saturation;
+    lightness;
 }
 export default class UnderlayLoader {
     load(bytes, id) {
@@ -18,8 +21,29 @@ export default class UnderlayLoader {
             this.handleOpcode(def, opcode, dataview);
         } while (opcode != 0);
 
-        def.color = this.convertToHsl(def.color);
+        this.loadHsl(def);
+        this.packHsl(def);
         return def;
+    }
+
+    packHsl(def) {
+        if (def.lightness > 179) {
+            def.saturation = (def.saturation / 2) | 0;
+        }
+      
+        if (def.lightness > 192) {
+            def.saturation = (def.saturation / 2) | 0;
+        }
+      
+        if (def.lightness > 217) {
+            def.saturation = (def.saturation / 2) | 0;
+        }
+      
+        if (def.lightness > 243) {
+            def.saturation = (def.saturation / 2) | 0;
+        }
+      
+        def.color = (Math.floor(def.saturation / 32) << 7) + (Math.floor(def.hue / 4) << 10) + (Math.floor(def.lightness / 2) | 0);
     }
 
     handleOpcode(def, opcode, dataview) {
@@ -31,11 +55,11 @@ export default class UnderlayLoader {
         }
     }
 
-    convertToHsl(var1) {
+    loadHsl(def) {
+        let var1 = def.color;
         let var2 = ((var1 >> 16) & 255) / 256;
         let var4 = ((var1 >> 8) & 255) / 256;
         let var6 = (var1 & 255) / 256;
-
         let var8 = var2;
         if (var4 < var2) {
             var8 = var4;
@@ -69,32 +93,32 @@ export default class UnderlayLoader {
             if (var2 == var10) {
                 var12 = (var4 - var6) / (var10 - var8);
             } else if (var4 == var10) {
-                var12 = (var6 - var2) / (var10 - var8) + 2;
+                var12 = 2 + (var6 - var2) / (var10 - var8);
             } else if (var6 == var10) {
                 var12 = 4 + (var2 - var4) / (var10 - var8);
             }
         }
 
         var12 /= 6;
-        let saturation = Math.floor(var14 * 8);
-        let lightness = Math.floor(var16 * 128);
+        let saturation = Math.floor(var14 * 256);
+        let lightness = Math.floor(var16 * 256);
         if (saturation < 0) {
             saturation = 0;
-        } else if (saturation > 7) {
-            saturation = 7;
+        } else if (saturation > 256) {
+            saturation = 256;
         }
 
         if (lightness < 0) {
             lightness = 0;
-        } else if (lightness > 127) {
-            lightness = 127;
+        } else if (lightness > 256) {
+            lightness = 256;
         }
 
         let hueMultiplier;
         if (var16 > 0.5) {
-            hueMultiplier = Math.floor((1 - var16) * var14 * 512);
+            hueMultiplier = Math.floor((var14 * (1 - var16)) * 512);
         } else {
-            hueMultiplier = Math.floor(512 * var16 * var14);
+            hueMultiplier = Math.floor(512 * (var16 * var14));
         }
 
         if (hueMultiplier < 1) {
@@ -103,6 +127,11 @@ export default class UnderlayLoader {
 
         let hue = Math.floor(hueMultiplier * var12);
 
-        return (hue << 10) | (saturation << 7) | lightness;
+
+        //console.log(`${var1} RGB ${(var1 >> 16) & 255}, ${(var1 >> 8) & 255}, ${var1 & 255}    HSL ${hue}, ${saturation}, ${lightness}`)
+
+        def.hue = hue;
+        def.saturation = saturation;
+        def.lightness = lightness;
     }
 }
